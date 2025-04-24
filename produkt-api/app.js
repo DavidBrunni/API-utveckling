6eae36bb50480fc925d00f3146ec9cd5a09ccc55
David Brunni <davidbrunni@hotmail.com>
2025-04-24 22:35:46 +0200

    uppdaterat med API kod
    Jag har nu lagt in allt jag behöver för projektet ska fungera. Jag har testat i postman och det verkar fungera som det ska.

--- produkt-api/app.js
+++ produkt-api/app.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.json());
 
 const categoriesRoutes = require('./routes/categoriesRoutes');
+const productsRoutes = require('./routes/productsRoutes');
+
 app.use('/categories', categoriesRoutes);
+app.use('/products', productsRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ produkt-api/controllers/productsController.js
@@ -0,0 +1,37 @@
+const db = require('../models/db');
+
+// Hämta alla produkter
+exports.getAllProducts = (req, res) => {
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+};
+
+// Skapa en ny produkt
+exports.createProduct = (req, res) => {
+  const { name, category_id, price } = req.body;
+  db.query('INSERT INTO products (name, category_id, price) VALUES (?, ?, ?)', [name, category_id, price], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).json({ id: result.insertId, name, category_id, price });
+  });
+};
+
+// Uppdatera produkt
+exports.updateProduct = (req, res) => {
+  const { id } = req.params;
+  const { name, category_id, price } = req.body;
+  db.query('UPDATE products SET name = ?, category_id = ?, price = ? WHERE id = ?', [name, category_id, price, id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.json({ message: 'Produkt uppdaterad' });
+  });
+};
+
+// Radera produkt
+exports.deleteProduct = (req, res) => {
+  const { id } = req.params;
+  db.query('DELETE FROM products WHERE id = ?', [id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.json({ message: 'Produkt raderad' });
+  });
+};
